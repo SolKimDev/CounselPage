9ffb1c308e1d1aca083957c530001c4aff602d97
Sol Kim <cjzl123475@gmail.com>
2021-10-29 16:30:37 +0900

    Install Router and styled-components. Set Font-Set, Mainpage Template and Palette Template without brand color. Memo Added. Using Noto Sans via Google Webfont.

---
+++ counsel-front/src/lib/styles/palette.js
@@ -0,0 +1,30 @@
+
+const palette = {
+    // 배열 순서 Heavy - Medium - Light
+    Font: [
+        '#191919',
+        '#767676',
+        '#999999'
+    ],
+    BG: [
+        '#F1F1F5',
+        '#F8F8FA'
+    ],
+    Line: [
+        '#191919',
+        '#DBDBDB',
+        '#EDEDED'
+    ],
+    // 브랜드색상 일반 - 마우스오버. 현재 브랜드색상 및 알람색상은 임시값임.
+    Brand_Main: [
+        '#6524FF',
+        '#D897FF'
+    ],
+    Brand_Sub : [
+        '#3A7BD8',
+        '#8DB7F3'
+    ],
+    Alert : '#FF3120'
+}
+
+export default palette;
