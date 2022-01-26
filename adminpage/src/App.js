7926189e6aa2c9820762ff95c250623b0f63bd2f
Sol Kim <cjzl123475@gmail.com>
2022-01-26 23:06:02 +0900

    feat: Set mainpage routing on App.js

--- adminpage/src/App.js
+++ adminpage/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Loginpage from "./pages/login/login";
+import Managingpage from "./pages/manage/manage";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // false로 하고 useEffect에서 체크하기.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {isLoggedIn ? (
+        <Route path="/" component={Managingpage} />
+      ) : (
+        <Route path="/" render={() => <Loginpage setIsLoggedIn />} />
+      )}
+    </Router>
   );
 }
 
