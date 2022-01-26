9dd4153f668adec14a703c64c2c5cec00c40302e
Sol Kim <cjzl123475@gmail.com>
2022-01-26 22:28:13 +0900

    feat: install "axios" and "polished" made basic code hierarchy.

---
+++ adminpage/src/lib/styles/palette.js
@@ -0,0 +1,39 @@
+
+const palette = {
+    // 배열 순서 Heavy - Medium - Light
+    Font: [
+        '#191919',
+        '#767676',
+        '#999999'
+    ],
+    BG: [
+        '#F1F1F5',
+        '#F8F8FA'
+    ],
+    Line: [
+        '#191919',
+        '#DBDBDB',
+        '#EDEDED'
+    ],
+    // 브랜드색상 일반 - 마우스오버. 현재 브랜드색상 및 알람색상은 임시값임.
+    Brand_Main: [
+        '#6524FF',
+        '#D897FF',
+        '#A444A4',
+        '#C36DC3'
+    ],
+    Brand_Sub : [
+        '#3A7BD8',
+        '#8DB7F3'
+    ],
+    Alert : '#FF3120',
+    Notice : [ //공지(dark/hover), 이벤트(dark/hover), 준비중 
+        '#3A7BD8',
+        '#5D92E1',
+        '#48EB35',
+        '#6AF05C',
+        '#999999'
+    ]
+}
+
+export default palette;
---
+++ adminpage/src/pages/manage/manage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const manage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default manage;
